d131b90c3b8cebbf470cbe00c1a88b845a6e1778
DeckerSU <deckersu@protonmail.com>
2023-11-10 17:02:52 +0100

    build: try to execute build from needed user

--- .github/actions/build-project-docker/entrypoint.sh
+++ .github/actions/build-project-docker/entrypoint.sh
@@ -162,7 +162,8 @@ groupadd --gid ${BUILDER_GID} --force ${BUILDER_NAME}
 adduser --disabled-password --gecos '' --no-create-home $BUILDER_NAME --uid ${BUILDER_UID} --gid ${BUILDER_GID}
 adduser $BUILDER_NAME sudo
 echo "$BUILDER_NAME ALL=(ALL:ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/$BUILDER_NAME
-su -m ${BUILDER_NAME}
+
+su -m $BUILDER_NAME << 'EOF'
 echo "User: $(whoami)"
 echo "Current directory: $(pwd)"
 
@@ -203,4 +204,6 @@ if false; then
 else
     emulate_build
 fi
+EOF
+
 
