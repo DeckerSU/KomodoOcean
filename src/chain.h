zcash/zcash

Add chain supply and transparent value to block index.
Co-authored-by: Jack Grigg <jack@z.cash>
Co-authored-by: Kris Nuttycombe <kris@nutty.land>
Co-authored-by: Daira Hopwood <daira@jacaranda.org>

src/chain.h
@@ -23,6 +23,7 @@ static const int SPROUT_VALUE_VERSION = 1001400;
 static const int SAPLING_VALUE_VERSION = 1010100;
 static const int CHAIN_HISTORY_ROOT_VERSION = 2010200;
 static const int NU5_DATA_VERSION = 4050000;
+static const int TRANSPARENT_VALUE_VERSION = 5040000;
 
 /**
  * Maximum amount of time that a block timestamp is allowed to be ahead of the
@@ -250,6 +251,34 @@ public:
     //! (memory only) The anchor for the tree state up to the end of this block
     uint256 hashFinalSproutRoot;
 
+    //! The change to the chain supply caused by this block. This is defined as
+    //! the value of the coinbase outputs in this block, minus fees not claimed
+    //! by the miner.
+    //!
+    //! Will be std::nullopt under the following conditions:
+    //! - if the block has never been connected to a chain tip
+    //! - for older blocks until a reindex has taken place
+    std::optional<CAmount> nChainSupplyDelta;
+
+    //! (memory only) Total chain supply up to and including this block.
+    //!
+    //! Will be std::nullopt until a reindex has taken place, if nChainTx is
+    //! zero, or if the block has never been connected to a chain tip.
+    std::optional<CAmount> nChainTotalSupply;
+
+    //! Change in value in the transparent pool produced by the action of the
+    //! transparent inputs to and outputs from transactions in this block.
+    //!
+    //! Will be std::nullopt for older blocks until a reindex has taken place.
+    std::optional<CAmount> nTransparentValue;
+
+    //! (memory only) Total value of the transparent value pool up to and
+    //! including this block.
+    //!
+    //! Will be std::nullopt until a reindex has taken place.
+    //! Will be std::nullopt if nChainTx is zero.
+    std::optional<CAmount> nChainTransparentValue;
+
     //! Change in value held by the Sprout circuit over this block.
     //! Will be std::nullopt for older blocks on old nodes until a reindex has taken place.
     std::optional<CAmount> nSproutValue;
@@ -342,6 +371,11 @@ public:
         hashFinalOrchardRoot = uint256();
         hashChainHistoryRoot = uint256();
         nSequenceId = 0;
+
+        nChainSupplyDelta = std::nullopt;
+        nChainTotalSupply = std::nullopt;
+        nTransparentValue = std::nullopt;
+        nChainTransparentValue = std::nullopt;
         nSproutValue = std::nullopt;
         nChainSproutValue = std::nullopt;
         nSaplingValue = 0;
@@ -538,6 +572,13 @@ public:
         READWRITE(nBits);
         READWRITE(nNonce);
         READWRITE(nSolution);
+
+        // Only read/write nTransparentValue if the client version used to create
+        // this index was storing them.
+        if ((s.GetType() & SER_DISK) && (nVersion >= TRANSPARENT_VALUE_VERSION)) {
+            READWRITE(nChainSupplyDelta);
+            READWRITE(nTransparentValue);
+        }
 
         // Only read/write nSproutValue if the client version used to create
         // this index was storing them.

src/rpc/blockchain.cpp
@@ -84,12 +84,14 @@ double GetNetworkDifficulty(const CBlockIndex* blockindex)
 }
 
 static UniValue ValuePoolDesc(
-    const std::string &name,
+    const std::optional<std::string> name,
     const std::optional<CAmount> chainValue,
     const std::optional<CAmount> valueDelta)
 {
     UniValue rv(UniValue::VOBJ);
-    rv.pushKV("id", name);
+    if (name.has_value()) {
+        rv.pushKV("id", name.value());
+    }
     rv.pushKV("monitored", (bool)chainValue);
     if (chainValue) {
         rv.pushKV("chainValue", ValueFromAmount(*chainValue));
@@ -265,8 +267,9 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool tx
     result.pushKV("difficulty", GetDifficulty(blockindex));
     result.pushKV("chainwork", blockindex->nChainWork.GetHex());
     result.pushKV("anchor", blockindex->hashFinalSproutRoot.GetHex());
-
+    result.pushKV("chainSupply", ValuePoolDesc(std::nullopt, blockindex->nChainTotalSupply, blockindex->nChainSupplyDelta));
     UniValue valuePools(UniValue::VARR);
+    valuePools.push_back(ValuePoolDesc("transparent", blockindex->nChainTransparentValue, blockindex->nTransparentValue));
     valuePools.push_back(ValuePoolDesc("sprout", blockindex->nChainSproutValue, blockindex->nSproutValue));
     valuePools.push_back(ValuePoolDesc("sapling", blockindex->nChainSaplingValue, blockindex->nSaplingValue));
     valuePools.push_back(ValuePoolDesc("orchard", blockindex->nChainOrchardValue, blockindex->nOrchardValue));
@@ -742,6 +745,23 @@ UniValue getblock(const UniValue& params, bool fHelp)
             "  \"nonce\" : n,           (numeric) The nonce\n"
             "  \"bits\" : \"1d00ffff\",   (string) The bits\n"
             "  \"difficulty\" : x.xxx,  (numeric) The difficulty\n"
+            "  \"chainSupply\": {          (object) information about the total supply\n"
+            "      \"monitored\": xx,           (boolean) true if the total supply is being monitored\n"
+            "      \"chainValue\": xxxxxx,      (numeric, optional) total chain supply\n"
+            "      \"chainValueZat\": xxxxxx,   (numeric, optional) total chain supply in satoshis\n"
+            "      \"valueDelta\": xxxxxx,      (numeric, optional) change to the chain supply produced by this block\n"
+            "      \"valueDeltaZat\": xxxxxx,   (numeric, optional) change to the chain supply produced by this block, in satoshis\n"
+            "  }\n"
+            "  \"valuePools\": [            (array) information about each value pool\n"
+            "      {\n"
+            "          \"id\": \"xxxx\",            (string) name of the pool\n"
+            "          \"monitored\": xx,           (boolean) true if the pool is being monitored\n"
+            "          \"chainValue\": xxxxxx,      (numeric, optional) total amount in the pool\n"
+            "          \"chainValueZat\": xxxxxx,   (numeric, optional) total amount in the pool in satoshis\n"
+            "          \"valueDelta\": xxxxxx,      (numeric, optional) change to the amount in the pool produced by this block\n"
+            "          \"valueDeltaZat\": xxxxxx,   (numeric, optional) change to the amount in the pool produced by this block, in satoshis\n"
+            "      }, ...\n"
+            "  ]\n"
             "  \"previousblockhash\" : \"hash\",  (string) The hash of the previous block\n"
             "  \"nextblockhash\" : \"hash\"       (string) The hash of the next block\n"
             "}\n"
@@ -1036,6 +1056,19 @@ UniValue getblockchaininfo(const UniValue& params, bool fHelp)
             "  \"chainwork\": \"xxxx\"     (string) total amount of work in active chain, in hexadecimal\n"
             "  \"size_on_disk\": xxxxxx,       (numeric) the estimated size of the block and undo files on disk\n"
             "  \"commitments\": xxxxxx,    (numeric) the current number of note commitments in the commitment tree\n"
+            "  \"chainSupply\": {          (object) information about the total supply\n"
+            "      \"monitored\": xx,           (boolean) true if the total supply is being monitored\n"
+            "      \"chainValue\": xxxxxx,      (numeric, optional) total chain supply\n"
+            "      \"chainValueZat\": xxxxxx,   (numeric, optional) total chain supply in satoshis\n"
+            "  }\n"
+            "  \"valuePools\": [            (array) information about each value pool\n"
+            "      {\n"
+            "          \"id\": \"xxxx\",            (string) name of the pool\n"
+            "          \"monitored\": xx,           (boolean) true if the pool is being monitored\n"
+            "          \"chainValue\": xxxxxx,      (numeric, optional) total amount in the pool\n"
+            "          \"chainValueZat\": xxxxxx,   (numeric, optional) total amount in the pool in satoshis\n"
+            "      }, ...\n"
+            "  ]\n"
             "  \"softforks\": [            (array) status of softforks in progress\n"
             "     {\n"
             "        \"id\": \"xxxx\",        (string) name of softfork\n"
@@ -1091,7 +1124,9 @@ UniValue getblockchaininfo(const UniValue& params, bool fHelp)
     obj.pushKV("commitments",           static_cast<uint64_t>(tree.size()));
 
     CBlockIndex* tip = chainActive.Tip();
+    obj.pushKV("chainSupply", ValuePoolDesc(std::nullopt, tip->nChainTotalSupply, std::nullopt));
     UniValue valuePools(UniValue::VARR);
+    valuePools.push_back(ValuePoolDesc("transparent", tip->nChainTransparentValue, std::nullopt));
     valuePools.push_back(ValuePoolDesc("sprout", tip->nChainSproutValue, std::nullopt));
     valuePools.push_back(ValuePoolDesc("sapling", tip->nChainSaplingValue, std::nullopt));
     valuePools.push_back(ValuePoolDesc("orchard", tip->nChainOrchardValue, std::nullopt));

DeckerSU/KomodoOcean

src/chain.h
@@ -37,6 +37,7 @@ extern CCriticalSection cs_main;
 
 static const int SPROUT_VALUE_VERSION = 80102;
 static const int SAPLING_VALUE_VERSION = 80102;
+static const int TRANSPARENT_VALUE_VERSION = 80103;
 
 // These 5 are declared here to avoid circular dependencies
 // code used this moved into .cpp
@@ -191,6 +192,34 @@ public:
     //! (memory only) The anchor for the tree state up to the end of this block
     uint256 hashFinalSproutRoot;
 
+    //! The change to the chain supply caused by this block. This is defined as
+    //! the value of the coinbase outputs in this block, minus fees not claimed
+    //! by the miner.
+    //!
+    //! Will be std::nullopt under the following conditions:
+    //! - if the block has never been connected to a chain tip
+    //! - for older blocks until a reindex has taken place
+    boost::optional<CAmount> nChainSupplyDelta;
+
+    //! (memory only) Total chain supply up to and including this block.
+    //!
+    //! Will be std::nullopt until a reindex has taken place, if nChainTx is
+    //! zero, or if the block has never been connected to a chain tip.
+    boost::optional<CAmount> nChainTotalSupply;
+
+    //! Change in value in the transparent pool produced by the action of the
+    //! transparent inputs to and outputs from transactions in this block.
+    //!
+    //! Will be std::nullopt for older blocks until a reindex has taken place.
+    boost::optional<CAmount> nTransparentValue;
+
+    //! (memory only) Total value of the transparent value pool up to and
+    //! including this block.
+    //!
+    //! Will be std::nullopt until a reindex has taken place.
+    //! Will be std::nullopt if nChainTx is zero.
+    boost::optional<CAmount> nChainTransparentValue;
+
     //! Change in value held by the Sprout circuit over this block.
     //! Will be boost::none for older blocks on old nodes until a reindex has taken place.
     boost::optional<CAmount> nSproutValue;
@@ -246,6 +275,11 @@ public:
         hashSproutAnchor = uint256();
         hashFinalSproutRoot = uint256();
         nSequenceId = 0;
+
+        nChainSupplyDelta = boost::none;
+        nChainTotalSupply = boost::none;
+        nTransparentValue = boost::none;
+        nChainTransparentValue = boost::none;
         nSproutValue = boost::none;
         nChainSproutValue = boost::none;
         nSaplingValue = 0;
@@ -439,6 +473,13 @@ public:
         READWRITE(nBits);
         READWRITE(nNonce);
         READWRITE(nSolution);
+
+        // Only read/write nTransparentValue if the client version used to create
+        // this index was storing them.
+        if ((s.GetType() & SER_DISK) && (nVersion >= TRANSPARENT_VALUE_VERSION)) {
+            READWRITE(nChainSupplyDelta);
+            READWRITE(nTransparentValue);
+        }
 
         // Only read/write nSproutValue if the client version used to create
         // this index was storing them.

src/rpc/blockchain.cpp
@@ -123,12 +123,14 @@ UniValue letsdebug(const UniValue& params, bool fHelp, const CPubKey& mypk) {
 }
 
 static UniValue ValuePoolDesc(
-    const std::string &name,
+    const boost::optional<std::string> name,
     const boost::optional<CAmount> chainValue,
     const boost::optional<CAmount> valueDelta)
 {
     UniValue rv(UniValue::VOBJ);
-    rv.push_back(Pair("id", name));
+    if (name.is_initialized()) {
+        rv.pushKV("id", name.value());
+    }
     rv.push_back(Pair("monitored", (bool)chainValue));
     if (chainValue) {
         rv.push_back(Pair("chainValue", ValueFromAmount(*chainValue)));
@@ -339,11 +341,11 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool tx
     result.push_back(Pair("difficulty", GetDifficulty(blockindex)));
     result.push_back(Pair("chainwork", blockindex->nChainWork.GetHex()));
     result.push_back(Pair("anchor", blockindex->hashFinalSproutRoot.GetHex()));
-    result.push_back(Pair("blocktype", "mined"));
-
+    result.pushKV("chainSupply", ValuePoolDesc(boost::none, blockindex->nChainTotalSupply, blockindex->nChainSupplyDelta));
     UniValue valuePools(UniValue::VARR);
-    valuePools.push_back(ValuePoolDesc("sprout", blockindex->nChainSproutValue, blockindex->nSproutValue));
-    valuePools.push_back(ValuePoolDesc("sapling", blockindex->nChainSaplingValue, blockindex->nSaplingValue));
+    valuePools.push_back(ValuePoolDesc(std::string("transparent"), blockindex->nChainTransparentValue, blockindex->nTransparentValue));
+    valuePools.push_back(ValuePoolDesc(std::string("sprout"), blockindex->nChainSproutValue, blockindex->nSproutValue));
+    valuePools.push_back(ValuePoolDesc(std::string("sapling"), blockindex->nChainSaplingValue, blockindex->nSaplingValue));
     result.push_back(Pair("valuePools", valuePools));
 
     if (blockindex->pprev)
@@ -858,6 +860,23 @@ UniValue getblock(const UniValue& params, bool fHelp, const CPubKey& mypk)
             "  \"nonce\" : n,           (numeric) The nonce\n"
             "  \"bits\" : \"1d00ffff\",   (string) The bits\n"
             "  \"difficulty\" : x.xxx,  (numeric) The difficulty\n"
+            "  \"chainSupply\": {          (object) information about the total supply\n"
+            "      \"monitored\": xx,           (boolean) true if the total supply is being monitored\n"
+            "      \"chainValue\": xxxxxx,      (numeric, optional) total chain supply\n"
+            "      \"chainValueZat\": xxxxxx,   (numeric, optional) total chain supply in satoshis\n"
+            "      \"valueDelta\": xxxxxx,      (numeric, optional) change to the chain supply produced by this block\n"
+            "      \"valueDeltaZat\": xxxxxx,   (numeric, optional) change to the chain supply produced by this block, in satoshis\n"
+            "  }\n"
+            "  \"valuePools\": [            (array) information about each value pool\n"
+            "      {\n"
+            "          \"id\": \"xxxx\",            (string) name of the pool\n"
+            "          \"monitored\": xx,           (boolean) true if the pool is being monitored\n"
+            "          \"chainValue\": xxxxxx,      (numeric, optional) total amount in the pool\n"
+            "          \"chainValueZat\": xxxxxx,   (numeric, optional) total amount in the pool in satoshis\n"
+            "          \"valueDelta\": xxxxxx,      (numeric, optional) change to the amount in the pool produced by this block\n"
+            "          \"valueDeltaZat\": xxxxxx,   (numeric, optional) change to the amount in the pool produced by this block, in satoshis\n"
+            "      }, ...\n"
+            "  ]\n"
             "  \"previousblockhash\" : \"hash\",  (string) The hash of the previous block\n"
             "  \"nextblockhash\" : \"hash\"       (string) The hash of the next block\n"
             "}\n"
@@ -1344,6 +1363,19 @@ UniValue getblockchaininfo(const UniValue& params, bool fHelp, const CPubKey& my
             "  \"chainwork\": \"xxxx\"     (string) total amount of work in active chain, in hexadecimal\n"
             "  \"size_on_disk\": xxxxxx,   (numeric) the estimated size of the block and undo files on disk\n"
             "  \"commitments\": xxxxxx,    (numeric) the current number of note commitments in the commitment tree\n"
+            "  \"chainSupply\": {          (object) information about the total supply\n"
+            "      \"monitored\": xx,           (boolean) true if the total supply is being monitored\n"
+            "      \"chainValue\": xxxxxx,      (numeric, optional) total chain supply\n"
+            "      \"chainValueZat\": xxxxxx,   (numeric, optional) total chain supply in satoshis\n"
+            "  }\n"
+            "  \"valuePools\": [            (array) information about each value pool\n"
+            "      {\n"
+            "          \"id\": \"xxxx\",            (string) name of the pool\n"
+            "          \"monitored\": xx,           (boolean) true if the pool is being monitored\n"
+            "          \"chainValue\": xxxxxx,      (numeric, optional) total amount in the pool\n"
+            "          \"chainValueZat\": xxxxxx,   (numeric, optional) total amount in the pool in satoshis\n"
+            "      }, ...\n"
+            "  ]\n"
             "  \"softforks\": [            (array) status of softforks in progress\n"
             "     {\n"
             "        \"id\": \"xxxx\",        (string) name of softfork\n"
@@ -1400,9 +1432,11 @@ UniValue getblockchaininfo(const UniValue& params, bool fHelp, const CPubKey& my
     obj.push_back(Pair("commitments",           static_cast<uint64_t>(tree.size())));
 
     CBlockIndex* tip = chainActive.Tip();
+    obj.pushKV("chainSupply", ValuePoolDesc(boost::none, tip->nChainTotalSupply, boost::none));
     UniValue valuePools(UniValue::VARR);
-    valuePools.push_back(ValuePoolDesc("sprout", tip->nChainSproutValue, boost::none));
-    valuePools.push_back(ValuePoolDesc("sapling", tip->nChainSaplingValue, boost::none));
+    valuePools.push_back(ValuePoolDesc(std::string("transparent"), tip->nChainTransparentValue, boost::none));
+    valuePools.push_back(ValuePoolDesc(std::string("sprout"), tip->nChainSproutValue, boost::none));
+    valuePools.push_back(ValuePoolDesc(std::string("sapling"), tip->nChainSaplingValue, boost::none));
     obj.push_back(Pair("valuePools",            valuePools));
 
     const Consensus::Params& consensusParams = Params().GetConsensus();